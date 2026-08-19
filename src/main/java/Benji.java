b7ce06ad5591bd21e5c4b56e2b903ad464ba51d1
Benjamin <wongjunweebenjamin@gmail.com>
2026-08-19 14:58:14 +0800

    add mark command

--- src/main/java/Benji.java
+++ src/main/java/Benji.java
@@ -32,6 +32,15 @@ public class Benji {
                 for (int i = 0; i < idx; i++) { // revealing all the tasks in the tasks array
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
+            } else if (userInput.toUpperCase().startsWith("MARK")) { // startsWith function to track command
+                // Integer.parseInt(...) converts the cleaned text string representing a number into an actual int
+                // .substring(5) skips the first 5 chars, 0 through 4
+                // .trim() cleans up and removes empty space chars from both the front and back
+                int taskNumber = Integer.parseInt(userInput.substring(5).trim());
+                Task task  = tasks[taskNumber - 1];
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + task);
             } else {
                 tasks[idx] = new Task(userInput); // adding a new task
                 idx++;
