bd7b0504673367c4b1e990c7235b0678e62113c1
Benjamin <wongjunweebenjamin@gmail.com>
2026-08-19 13:55:51 +0800

    Add support for Echo

--- src/main/java/Benji.java
+++ src/main/java/Benji.java
@@ -1,6 +1,7 @@
 /**
  * Starts the BENJI chatbot application.
  */
+import java.util.Scanner;
 public class Benji {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -11,13 +12,25 @@ public class Benji {
                 + "  BB  BB  EE       NN  NNN  JJ JJJ     III  \n"
                 + "  BBBBB   EEEEEEE  NN   NN   JJJJJ   IIIIIII\n";
 
+        Scanner scanner = new Scanner(System.in); // create a tool or scanner that reads input typed by a user
         System.out.println(line);
         System.out.println(banner);
-        System.out.println("Hello! I'm BENJI");
+        System.out.println("H#ello! I'm BENJI");
         System.out.println("What can I do for you?");
         System.out.println(line);
+        while(true) {
+            String userInput = scanner.nextLine(); // waits for a user to type a full line of text and press Enter.
+            if (userInput.toUpperCase().equals("BYE")) {
+                break;
+            }
+            System.out.println(userInput);
+            System.out.println(line);
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
+
+
 
     }
 }
