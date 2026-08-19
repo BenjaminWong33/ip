8512fdbf7dc45fb37a644d68b495a7119c066111
Benjamin <wongjunweebenjamin@gmail.com>
2026-08-19 14:18:50 +0800

    Add list

--- src/main/java/Benji.java
+++ src/main/java/Benji.java
@@ -13,9 +13,13 @@ public class Benji {
                 + "  BBBBB   EEEEEEE  NN   NN   JJJJJ   IIIIIII\n";
 
         Scanner scanner = new Scanner(System.in); // create a tool or scanner that reads input typed by a user
+        String[] tasks = new String[100]; // to store all tasks
+        int idx = 0;  // index for tasks
+
+
         System.out.println(line);
         System.out.println(banner);
-        System.out.println("H#ello! I'm BENJI");
+        System.out.println("Hello! I'm BENJI");
         System.out.println("What can I do for you?");
         System.out.println(line);
         while(true) {
@@ -23,7 +27,19 @@ public class Benji {
             if (userInput.toUpperCase().equals("BYE")) {
                 break;
             }
-            System.out.println(userInput);
+
+            if (userInput.toUpperCase().equals("LIST")) {
+                for (int i = 0; i < idx; i++) { // revealing all the tasks in the tasks array
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[idx] = userInput; // adding a new task
+                idx++;
+                System.out.println("Added: " + userInput);
+
+            }
+
+
             System.out.println(line);
         }
 
