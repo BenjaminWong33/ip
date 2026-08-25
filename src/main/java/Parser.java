7ae3964fd757c4fb6264d30f688e7a26813e7551
Benjamin <wongjunweebenjamin@gmail.com>
2026-08-25 16:32:14 +0800

    Add Parser.java

---
+++ src/main/java/Parser.java
@@ -0,0 +1,24 @@
+public class Parser {
+    private static Command getCommand(String input) { // get command function, map it to enum
+        String upperInput = input.toUpperCase();
+        if (upperInput.equals("BYE")) {
+            return Command.BYE;
+        } else if (upperInput.equals("LIST")) {
+            return Command.LIST;
+        } else if (upperInput.startsWith("MARK ")) {
+            return Command.MARK;
+        } else if (upperInput.startsWith("UNMARK ")) {
+            return Command.UNMARK;
+        } else if (upperInput.startsWith("TODO")) {
+            return Command.TODO;
+        } else if (upperInput.startsWith("DEADLINE")) {
+            return Command.DEADLINE;
+        } else if (upperInput.startsWith("EVENT")) {
+            return Command.EVENT;
+        } else if (upperInput.startsWith("DELETE ")) {
+            return Command.DELETE;
+        } else {
+            return Command.UNKNOWN;
+        }
+    }
+}
