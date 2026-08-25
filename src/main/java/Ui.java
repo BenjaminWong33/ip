ad8acb6c9111203381e6567bd909a3340afd5e5d
Benjamin <wongjunweebenjamin@gmail.com>
2026-08-25 16:28:54 +0800

    Add Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ui {
+    String line = "____________________________________________________________";
+    String banner = ""
+            + "  BBBBB   EEEEEEE  NN   NN  JJJJJJJ  IIIIIII\n"
+            + "  BB  BB  EE       NNN  NN     JJJ     III  \n"
+            + "  BBBBB   EEEEE    NN N NN     JJJ     III  \n"
+            + "  BB  BB  EE       NN  NNN  JJ JJJ     III  \n"
+            + "  BBBBB   EEEEEEE  NN   NN   JJJJJ   IIIIIII\n";
+
+    private Scanner scanner;
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm BENJI.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+    }
+
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    public void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void showError(String message) {
+        System.out.println(message);
+    }
+}
