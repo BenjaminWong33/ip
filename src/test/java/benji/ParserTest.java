492284a2fd004d49e350ae45a21511a6155b12c2
Benjamin <wongjunweebenjamin@gmail.com>
2026-09-14 15:20:21 +0800

    Add more tests to ParserTest

--- src/test/java/benji/ParserTest.java
+++ src/test/java/benji/ParserTest.java
@@ -18,4 +18,25 @@ public class ParserTest {
         assertEquals(Command.UNKNOWN, command);
     }
 
+    @Test
+    public void parseHelpCommand_acceptsOnlyExactHelpCommand() {
+        assertEquals(Command.HELP, Parser.getCommand("help"));
+        assertEquals(Command.HELP, Parser.getCommand("Help"));
+        assertEquals(Command.HELP, Parser.getCommand("HeLp"));
+        assertEquals(Command.UNKNOWN, Parser.getCommand("Help me"));
+    }
+
+    @Test
+    public void parseCommands_returnsMatchingCommands() {
+        assertEquals(Command.LIST, Parser.getCommand("list"));
+        assertEquals(Command.MARK, Parser.getCommand("mark 1"));
+        assertEquals(Command.UNMARK, Parser.getCommand("unmark 1"));
+        assertEquals(Command.DEADLINE,
+                Parser.getCommand("deadline submit report /by 2026-09-14"));
+        assertEquals(Command.EVENT,
+                Parser.getCommand("event meeting /from 2pm /to 3pm"));
+        assertEquals(Command.DELETE, Parser.getCommand("delete 1"));
+        assertEquals(Command.FIND, Parser.getCommand("find homework"));
+        assertEquals(Command.BYE, Parser.getCommand("bye"));
+    }
 }
